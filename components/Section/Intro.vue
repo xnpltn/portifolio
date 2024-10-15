e47614bb4a519d633e8b6b5f3c62034d2e71cbfb
Platon <xnpltn@gmail.com>
2024-10-15 04:34:22 +0200

    Fixes for Strong component

--- components/Section/Intro.vue
+++ components/Section/Intro.vue
@@ -10,19 +10,19 @@ const BlogLink = h(NuxtLink, {to: "/blog"})
   </div>
   <div class="hero__intro">
     <p class="hero__text">
-      Hi, I am <strong>Platon</strong>
+      Hi, I am <Strong>Platon</Strong>
       <span class="italic__text">&nbsp;&nbsp;read /pla.tɔ̃/ &nbsp; not &nbsp; /play-ton/</span><br />
       Don't know how you landed here but at least someone is watching my ugly site lol.
     </p>
 
     <p class="hero__text">
-      Building things is what I enjoy. Interested in <strong>Backend</strong> and <strong>Terminal Applications.</strong> &nbsp;
+      Building things is what I enjoy. Interested in <Strong>Backend</Strong> and <Strong>Terminal Applications.</Strong> &nbsp;
       Anything Written in: <br />
-      <strong>Go</strong> (<span class="italic__text">My Favourite</span>), &nbsp; <strong>Rust</strong>, &nbsp; <strong>C</strong>, &nbsp;<br />
-      <strong>Python</strong>, &nbsp; and <strong>Javascript</strong> &nbsp; sign me up :/
+      <Strong>Go</Strong> (<span class="italic__text">My Favourite</span>), &nbsp; <Strong>Rust</Strong>, &nbsp; <Strong>C</Strong>, &nbsp;<br />
+      <Strong>Python</Strong>, &nbsp; and <Strong>Javascript</Strong> &nbsp; sign me up :/
     </p>
 
-    <p><strong>VueJs</strong> is the best web framework of all time btw!</p>
+    <p><Strong>VueJs</Strong> is the best web framework of all time btw!</p>
     <div class="hero__buttons">
       <Button as="a" href="https://github.com/xnpltn" target="_blank">Github</Button>
       <Button :as="BlogLink">Read my blog</Button>
